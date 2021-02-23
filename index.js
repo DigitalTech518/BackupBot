739000febe06a7f7a24cdba0a2af19ff76409e2b
DigitalTech518 <75098262+DigitalTech518@users.noreply.github.com>
2021-02-22 21:31:30 -0600

    Mep

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const config = require("./config.js")
 const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+
 const client = new Discord.Client();
 
 client.once("ready", () => {
 	console.log("Backup Bot Ready!");
 });
 
-client.login(config.token);
+client.on("message", message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	if (command === "meep") {
+		message.channel.send("Meep?");
+	}
+	else if (command === "ping") {
+		message.channel.send(`Latency is ${Math.round(client.ws.ping)}ms`)
+	}
+
+});	
+
+client.login(token);
